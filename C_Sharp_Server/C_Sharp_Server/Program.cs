97e087a500939195153f6f280cdb31d470c6332f
ambosing <ambosing_@naver.com>
2024-12-09 18:00:55 +0900

    git init

---
+++ C_Sharp_Server/C_Sharp_Server/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace C_Sharp_Server;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ C_Sharp_Server/DummyClient/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DummyClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
