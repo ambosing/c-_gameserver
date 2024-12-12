1be0db63c52452f6e2ea420e2d842bd715c8a761
Yang JiWon <ambosing_@naver.com>
2024-12-12 18:44:34 +0900

    원자적 연산으로 Race Condition인 공유자원 문제 해결

--- C_Sharp_Server/ServerCore/Program.cs
+++ C_Sharp_Server/ServerCore/Program.cs
@@ -2,27 +2,37 @@
 
 class Program
 {
-    
+    static int number = 0; // Race Condition
+
+    static void Thread_1()
+    {
+        for (int i = 0; i < 100000; i++)
+        {
+            int prev = number;
+            Console.WriteLine($"prev:{prev}");
+            Interlocked.Increment(ref number); // 성능에서 손해 봄, 대신 원자성 보장
+            int next = number;
+            Console.WriteLine($"next:{next}");
+        }
+
+    }
+    static void Thread_2()
+    {
+        for (int i = 0; i < 100000; i++)
+            Interlocked.Decrement(ref number); // 원자성 보장
+        // 메모리 배리어를 간접적으로 사용하고 있어서 volatile을 쓰지 않아도 됨
+    }
+
     static void Main(string[] args)
     {
-        int[,] arr = new int[10000, 10000];
+        Task t1 = new Task(Thread_1);
+        Task t2 = new Task(Thread_2);
 
-        {
-            long now = DateTime.Now.Ticks;
-            for (int y = 0; y < 10000; y++)
-                for (int x = 0; x < 10000; x++)
-                    arr[y, x] = 1;
-            long end = DateTime.Now.Ticks;
-            Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
-        }
+        t1.Start();
+        t2.Start();
 
-        {
-            long now = DateTime.Now.Ticks;
-            for (int y = 0; y < 10000; y++)
-                for (int x = 0; x < 10000; x++)
-                    arr[x, y] = 1;
-            long end = DateTime.Now.Ticks;
-            Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
-        }
+        Task.WaitAll(t1, t2);
+
+        Console.WriteLine(number);
     }
 }
